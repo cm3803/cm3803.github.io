bcbe57cfdc4535de6f489336d32a6f51f9019b2d
cate01px2021 <catemok@Cates-MacBook-2.local>
2022-01-15 17:01:34 -0500

    end of JADE

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,15 @@ $(document).ready(function(){
 	var myFavoriteBook = "A Raisin in the Sun";
 	console.log("My favorite book is " +myFavoriteBook)
 
-	/* call this function when clicked*/
+	$("body p").highlight("Barnard (class of 2025)", { className: 'highlight' });
+	$("body p").highlight("Architecture", { className: 'highlight' });
+	// specify class name (case sensitive)
+	// $("body p").highlight("Barnard", { className: 'youtube', caseSensitive: true });
+
+	$(document).ready(function() {
+   // unnecessary if you load all your scripts at the bottom of your page
+	});
+	/* call this function when clicked
 
 	window.onclick = changeBG;
 
@@ -13,6 +21,8 @@ $(document).ready(function(){
 		document.getElementsByTagName("Body")[0].style.backgroundColor = "#C1E3B0";
 		document.getElementsByTagName("Body")[0].style.color="pink";
 	}
+
+	*/
 
 
 
